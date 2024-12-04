8554614f2be4aa7eb45f0e124ef59c72432e6323
Hbuhero <hbuhero@gmail.com>
2024-12-04 23:40:27 +0300

    tried minting

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,30 +1,89 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.19;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "@openzeppelin/contracts/";
+
 /**
  * @title DSCEngine
  * @author Hud Saidi
- * 
+ *
  * This system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg
  * This stablecoin has the properties:
  * - Exogenous Collateral
  * - Dollar Pegged
  * - Algorithmic Stable
- * 
+ *
  * It is similar to DAI if DAI had no fees and only backed by WETH and WBTC
- * 
+ *
  * Our DSC system should always be overcollateralized. At no point, should the value of all collateral <= the $ backed value of all the DSC
- * 
- * @notice This contract is the core of the DSC system. It handles all the logic for mining and 
+ *
+ * @notice This contract is the core of the DSC system. It handles all the logic for mining and
  * redeeming DSC, as well as depositing & withdrawing collateral
  * @notice THis contract is VERY loosely based on the MAKERDAO DSS (DAI) system.
  */
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeOfTheSameLength();
+    error DSCEngine__NotAllowedToken();
+    error DSCEngine__TransferFailed();
+
+    event CollateralDeposited(address indexed sender, address indexed token, uint256 indexed amount);
+
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+    DecentralizedStableCoin private immutable i_dscAddress;
+    uint256 private constant LIQUIDATION_THRESHOLD = 
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAdresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAdresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeOfTheSameLength();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAdresses[i];
+        }
+
+        i_dscAddress = DecentralizedStableCoin(dscAddress);
+    }
+
     function depositCollateralAndMintDsc() external {}
 
-    function depositCollateral() external {}
+    /*
+     * @param tokenCollateralAddress - The address of the token to deposit as collateral
+     * @param amountCollateral - The amount of collateral to deposit
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
 
-    function redeemCollateralForDsc() external{}
+    function redeemCollateralForDsc() external {}
 
     function redeemCollateral() external {}
 
@@ -35,4 +94,42 @@ contract DSCEngine {
     function liquidate() external {}
 
     function getHealthFactor() external {}
+
+
+
+
+    // check collateral for the sender
+    // calculate the health factor of the mint
+    // if health factor is low revert else continue
+    // mint dsc
+    function mintByMe(uint256 amount, address tokenAddress) external {
+        if (s_collateralDeposited[msg.sender][tokenAddress] <= 0){
+            revert();
+        }
+
+        
+    }
+
+
+
+    function getPrice(
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        // Sepolia ETH / USD Address
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
+    }
+
+    // 1000000000
+    function getConversionRate(
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice(priceFeed);
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+        return ethAmountInUsd;
+    }
 }
